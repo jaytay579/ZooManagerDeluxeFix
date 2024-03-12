7851fae290caade629fac9bd5d0fe811507f1c32
Laird <j.laird@CPSYHF43F12P6.tld>
2024-03-12 16:42:25 -0400

    Initial Commit

---
+++ ZooManagerFixAttempt2/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ZooManager
+{
+    public class Animal : Occupant
+    {
+        public string name;
+        public int reactionTime = 5; // default reaction time for animals (for this game, range is 1 - 9, with 10 being dead)
+        public bool acted = false; // has the animal acted yet this turn?
+
+        /* "virtual" indicates that this method can be overridden by a method of the same name in classes descended from Animal */
+        virtual public void Activate()
+        {
+            Console.WriteLine($"Animal {name} at {location.x},{location.y} activated");
+        }
+
+        public void SlowDown()
+        {
+            reactionTime++;
+            if (reactionTime > 10)
+            {
+                reactionTime = 10;
+            }
+            Console.WriteLine($"Animal {name} slowed to {reactionTime}");
+        }
+
+        public void SpeedUp()
+        {
+            reactionTime--;
+            if (reactionTime < 1)
+            {
+                reactionTime = 1;
+            }
+        }
+    }
+}
---
+++ ZooManagerFixAttempt2/Cat.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ZooManager
+{
+    public class Cat : Animal
+    {
+        public Cat(string name)
+        {
+            emoji = "🐱";
+            species = "cat";
+            this.name = name;
+            reactionTime = new Random().Next(1, 6); // reaction time 1 (fast) to 5 (medium)Cat
+        }
+
+        public override void Activate()
+        {
+            base.Activate(); // First call the Activate method from our ancestor class...
+            // Console.WriteLine("I am a cat. Meow.");
+            if (!Hunt())
+            {
+                SlowDown();
+            }
+            else
+            {
+                SpeedUp();
+            }
+        }
+
+        /* Note that our cat is currently not very clever about its hunting.
+         * It will always try to attack "up" and will only seek "down" if there
+         * is no mouse above it. This does not affect the cat's effectiveness
+         * very much, since the overall logic here is "look around for a mouse and
+         * attack the first one you see." This logic might be less sound once the
+         * cat also has a predator to avoid, since the cat may not want to run in
+         * to a square that sets it up to be attacked!
+         */
+        public bool Hunt()
+        {
+            if (Game.Seek(location.x, location.y, Direction.up, "mouse"))
+            {
+                Game.Attack(this, Direction.up);
+                return true;
+            }
+            else if (Game.Seek(location.x, location.y, Direction.down, "mouse"))
+            {
+                Game.Attack(this, Direction.down);
+                return true;
+            }
+            else if (Game.Seek(location.x, location.y, Direction.left, "mouse"))
+            {
+                Game.Attack(this, Direction.left);
+                return true;
+            }
+            else if (Game.Seek(location.x, location.y, Direction.right, "mouse"))
+            {
+                Game.Attack(this, Direction.right);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
